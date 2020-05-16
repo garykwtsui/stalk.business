14e2d3e6861ffeee1d575cc2c85c1339a81c5326
Gary K. W. Tsui <garykwtsui@gmail.com>
2020-05-15 19:14:33 -0700

    front end update

---
+++ frontend/build/precache-manifest.cb4890366f73ac704e4a18d6faa5947a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76c3a80783f20733a0f4639ed16b8f7",
+    "url": "/stalk.business/index.html"
+  },
+  {
+    "revision": "69a6b54939d29d31126e",
+    "url": "/stalk.business/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90a9c5e38b3f0407c6a4",
+    "url": "/stalk.business/static/js/2.d7d394be.chunk.js"
+  },
+  {
+    "revision": "33ad966c50f3e315fdc82eaebd9dfc8e",
+    "url": "/stalk.business/static/js/2.d7d394be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a6b54939d29d31126e",
+    "url": "/stalk.business/static/js/main.31376a8a.chunk.js"
+  },
+  {
+    "revision": "a229c89211270d73a6b2",
+    "url": "/stalk.business/static/js/runtime-main.55500822.js"
+  },
+  {
+    "revision": "6583b7bda57f622c015560fbd02032be",
+    "url": "/stalk.business/static/media/computerError2.6583b7bd.mp3"
+  },
+  {
+    "revision": "2d52465526f8bf55df97311dafb967e2",
+    "url": "/stalk.business/static/media/ding.2d524655.mp3"
+  },
+  {
+    "revision": "31a564d2ed616358d7a423fff0896b74",
+    "url": "/stalk.business/static/media/waterdrop2.31a564d2.mp3"
+  }
+]);
