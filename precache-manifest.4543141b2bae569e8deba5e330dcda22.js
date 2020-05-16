777739762bdda54dad8503d828e0ecf4f3e9c072
Gary K. W. Tsui <garykwtsui@gmail.com>
2020-05-16 15:35:30 -0700

    Updates

--- precache-manifest.4543141b2bae569e8deba5e330dcda22.js
+++ precache-manifest.4543141b2bae569e8deba5e330dcda22.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dbdd39d2fae7446a3c35ec4aa9071e27",
+    "revision": "448ef21fafb596039be1372276fe223a",
     "url": "/stalk.business/index.html"
   },
   {
-    "revision": "f09c566a4da8dbc0343a",
+    "revision": "288efec3cf221685f3dc",
     "url": "/stalk.business/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "260bb5ce59d33cab2e22",
-    "url": "/stalk.business/static/js/2.50c8f796.chunk.js"
+    "revision": "c0fb63922779e1f1b6c6",
+    "url": "/stalk.business/static/js/2.297d49fd.chunk.js"
   },
   {
     "revision": "33ad966c50f3e315fdc82eaebd9dfc8e",
-    "url": "/stalk.business/static/js/2.50c8f796.chunk.js.LICENSE.txt"
+    "url": "/stalk.business/static/js/2.297d49fd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f09c566a4da8dbc0343a",
-    "url": "/stalk.business/static/js/main.7ce87d88.chunk.js"
+    "revision": "288efec3cf221685f3dc",
+    "url": "/stalk.business/static/js/main.f800736a.chunk.js"
   },
   {
     "revision": "a229c89211270d73a6b2",
